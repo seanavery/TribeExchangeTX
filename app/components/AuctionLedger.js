b6d0e3ebbd1642fe8c6e7be5b0d1bb77bf28b789
Sean Pollock <seanpollock3344@gmail.com>
2016-10-06 16:46:30 -0500

    adding ledger container and view structure

--- app/components/AuctionLedger.js
+++ app/components/AuctionLedger.js
@@ -4,6 +4,8 @@ import {render} from 'react-dom';
 
 class AuctionLedger extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return <p> hello form auction</p>
   }
 }
+
+module.exports = AuctionLedger;
