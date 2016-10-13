3fc6f8e8457142335f15b8ee44de5bbe353fb3ba
Sean Pollock <seanpollock3344@gmail.com>
2016-10-13 15:58:10 -0500

    adding css files for reactbootstrap

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AuctionLedger from './components/AuctionLedger'
-import ClearingLedger from './components/ClearingLedger'
+import BidBox from './components/BidBox'
+import AskBox from './components/AskBox'
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <AuctionLedger type="Bids" />
-        <AuctionLedger type="Asks" />
-        <ClearingLedger />
+        <BidBox />
+        <AskBox />
       </div>
     )
   }
