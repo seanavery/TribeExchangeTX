b2166cfd27c7c9830c69176c07131bf9aecd640b
Sean Pollock <seanpollock3344@gmail.com>
2016-10-13 15:52:20 -0500

    re-organizing front end with BidBox and AskBox

---
+++ app/components/AskBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+class AskBox extends React.Component {
+  render () {
+    return (
+      <div>
+        Ask Box
+      </div>
+    )
+  }
+}
+
+module.exports = AskBox
---
+++ app/components/BidBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+class BidBox extends React.Component {
+  render () {
+    return (
+      <div>
+        Bid Box
+      </div>
+    )
+  }
+}
+
+module.exports = BidBox
