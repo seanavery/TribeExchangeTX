56ae2e286f5dc0f6e4194bc997e0def8efe319d3
Sean Pollock <seanpollock3344@gmail.com>
2016-10-13 16:56:57 -0500

    adding navbar

---
+++ app/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {render} from 'react-dom'
+// import {Navbar} from 'react-bootstrap'
+import  { Navbar } from 'react-bootstrap'
+// require("react-bootstrap/lib/NavbarHeader")
+
+
+class NavBar extends React.Component {
+  render () {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Tribe Exchange</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>
+    )
+  }
+}
+
+module.exports = NavBar
--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {render} from 'react-dom';
 import BidBox from './components/BidBox'
 import AskBox from './components/AskBox'
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
   render () {
     return (
       <div>
+        <NavBar />
         <BidBox />
         <AskBox />
       </div>
