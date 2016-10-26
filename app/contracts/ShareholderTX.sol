f75064737ae87f9f7dacad4c86d6b410579e9bce
Sean Pollock <seanpollock3344@gmail.com>
2016-10-25 23:06:46 -0500

    sufficientAssets modifier and updated transferShares function

--- app/contracts/ShareholderTX.sol
+++ app/contracts/ShareholderTX.sol
@@ -12,20 +12,24 @@ contract ShareholderAgreement {
     function ShareholderAgreement(uint _totalShares) {
         // will likely support customized implementation
         totalShares = _totalShares;
+        Shareholders[msg.sender].amount = _totalShares;
     }
 
-    // modifier sufficientAssets(uint _amount) {
-    //     if(Shareholders[msg.sender].amount <  _amount) throw;
-    //     _;
-    // }
+    modifier sufficientAssets(uint _amount) {
+        if(Shareholders[msg.sender].amount <  _amount) throw;
+        _;
+    }
 
-    function transferShares(uint _amount) returns(bool) {
-        Shareholders[msg.sender].amount = _amount;
-        Shareholders[msg.sender].date = now;
-        return true;
+    function transferShares(uint _amount, address _send) sufficientAssets(_amount) returns(bool) {
+           Shareholders[msg.sender].amount -= _amount;
+           Shareholders[msg.sender].date = now;
+           Shareholders[_send].amount += _amount;
+           Shareholders[_send].date = now;
+           return true;
     }
 
     function getAddress() returns(address) {
         return msg.sender;
     }
+
 }
