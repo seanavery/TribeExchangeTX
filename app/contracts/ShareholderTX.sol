08e18bdd72c2655fd7d3a3e74d87ff09b55a89a2
Sean Pollock <seanpollock3344@gmail.com>
2016-10-25 22:17:21 -0500

    Standard Token wont due --> starting custom sharholder contract

---
+++ app/contracts/ShareholderTX.sol
@@ -0,0 +1,27 @@
+contract ShareholderAgreement {
+
+    uint totalShares;
+
+    struct Shareholder {
+        uint amount;
+        uint date;
+    }
+
+    mapping(address=>Shareholder) public Shareholders;
+
+    function ShareholderAgreement(uint _totalShares) {
+        // will likely support customized implementation
+        totalShares = _totalShares;
+    }
+
+    // modifier sufficientAssets(uint _amount) {
+    //     if(Shareholders[msg.sender].amount <  _amount) throw;
+    //     _;
+    // }
+
+    function transferShares(uint _amount) returns(bool) {
+        Shareholders[msg.sender].amount = _amount;
+        Shareholders[msg.sender].date = now;
+        return true;
+    }
+}
