1409b20dc28d6b3cad5e3901d77218c6da2447ba
Sean Pollock <seanpollock3344@gmail.com>
2016-11-06 14:36:13 -0600

    reading contractsTX directory

--- build/ContractBuild.js
+++ build/ContractBuild.js
@@ -1,5 +1,28 @@
 const fs = require('fs')
+const testFolder = './contractsTX'
 
-fs.readdir('../contractsTX', (err, files) => {
-  console.log(files);
+// fs.readdir(contractFolder, (err, files) => {
+//   console.log(files);
+//   files.forEach(file => {
+//     console.log(file)
+//   })
+// })
+
+fs.readdir(testFolder, (err, files) => {
+  if(!err) {
+    files.forEach(file => {
+      console.log(file)
+    })
+  }
+  else {
+    console.log(err)
+  }
 })
+
+// const testFolder = './tests/';
+// const fs = require('fs');
+// fs.readdir(testFolder, (err, files) => {
+//   files.forEach(file => {
+//     console.log(file);
+//   });
+// })
