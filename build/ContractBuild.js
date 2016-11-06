b94c597f8292b94f84880ef3bb4e7441ddd49d3b
Sean Pollock <seanpollock3344@gmail.com>
2016-11-06 15:08:52 -0600

    solc compiles .sol contracts

--- build/ContractBuild.js
+++ build/ContractBuild.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const testFolder = './contractsTX'
+const solc = require('solc')
+
 
 // fs.readdir(contractFolder, (err, files) => {
 //   console.log(files);
@@ -7,23 +9,23 @@ const testFolder = './contractsTX'
 //     console.log(file)
 //   })
 // })
-var contract_strings = []
 
 fs.readdir(testFolder, (err, files) => {
   if(!err) {
     files.forEach(file => {
       console.log(file)
-      fs.readFile(file, (err, data) => {
+      var filename = './contractsTX/' + file
+      fs.readFile(filename, 'utf-8', (err, data) => {
         if(!err) {
-          console.log(data)
-          contract_strings.push(data)
+          // console.log(data)
+          var output = solc.compile(data, 1)
+          console.log(output.contracts)
         } else {
           console.log(err)
         }
       })
     })
-  }
-  else {
+  } else {
     console.log(err)
   }
 })
