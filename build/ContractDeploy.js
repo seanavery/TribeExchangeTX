393fd7fd48539474098462a675252421048b3386
Sean Pollock <seanpollock3344@gmail.com>
2016-11-07 11:54:02 -0600

    web3 provider at localhost 8545

--- build/ContractDeploy.js
+++ build/ContractDeploy.js
@@ -1,9 +1,17 @@
 var TestRPC = require("ethereumjs-testrpc");
+var Web3 = require("web3")
+var web3 = new Web3();
 var server = TestRPC.server();
+
 server.listen(8545, function(err, blockchain) {
   if(!err) {
     console.log('test rpc started on port 8545')
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+console.log('made ithere')
+    var coinbase = web3.eth.coinbase;
+
+    console.log(coinbase)
   } else {
     console.log(err)
   }
-});
+})
