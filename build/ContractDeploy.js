450c06c8a3c520902adbb5c4854f71801fed79ee
Sean Pollock <seanpollock3344@gmail.com>
2016-11-09 20:12:00 -0600

    web 3 with testrpc)

--- build/ContractDeploy.js
+++ build/ContractDeploy.js
@@ -1,17 +1,27 @@
 var TestRPC = require("ethereumjs-testrpc");
+var Promise = require("bluebird");
 var Web3 = require("web3")
 var web3 = new Web3();
-var server = TestRPC.server();
+// var server = TestRPC.server();
+web3.setProvider(TestRPC.provider());
 
-server.listen(8545, function(err, blockchain) {
-  if(!err) {
-    console.log('test rpc started on port 8545')
-    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
-console.log('made ithere')
-    var coinbase = web3.eth.coinbase;
+var eth = Promise.promisifyAll(web3.eth);
 
-    console.log(coinbase)
-  } else {
-    console.log(err)
-  }
-})
+// server.listen(8545, function(err, blockchain) {
+//   if(!err) {
+//     console.log('test rpc started on port 8545')
+// //     web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+// // console.log('made ithere')
+// //     var coinbase = web3.eth.coinbase;
+// //
+// //     console.log(coinbase)
+//   } else {
+//     console.log(err)
+//   }
+// })
+
+var coinbase = eth.coinbase;
+console.log(coinbase)
+// web3.eth.getBalance(web3.eth.coinbase, (err, res) => {
+//   console.log(err + res)
+// })
